385679aefe20c69a1ec9c078593882725efb9b2a
dwkdania-dot <dwk.dania@gmail.com>
2026-05-02 17:03:06 +0500

    Add Student class with name and sapID fields

---
+++ Student.java
@@ -0,0 +1,8 @@
+
+public class Student {
+    private String name = "Placeholder Student";
+    private int sapID = 00000;
+
+    public String getName() { return name; }
+    public int getSapID() { return sapID; }
+}
