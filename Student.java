9e33762fe6dbcbffd1e6051b45d6fe43a7d513f1
dwkdania-dot <dwk.dania@gmail.com>
2026-05-04 14:04:06 +0500

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,8 +1,38 @@
+package users;
 
-public class Student {
-    private String name = "Placeholder Student";
-    private int sapID = 00000;
+import java.util.ArrayList;
+import java.util.List;
+import university.Course;
 
-    public String getName() { return name; }
-    public int getSapID() { return sapID; }
+public class Student extends Person {
+    private String studentID;
+    private double gpa;
+    private List<Course> enrolledCourses;
+
+    public Student(String name, int age, String cnic, String studentID) {
+        super(name, age, cnic);
+        this.studentID = studentID;
+        this.enrolledCourses = new ArrayList<>();
+    }
+
+    public void registerCourse(Course c) {
+        enrolledCourses.add(c);
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("Student: " + name + " | ID: " + studentID + " | GPA: " + gpa);
+    }
 }
